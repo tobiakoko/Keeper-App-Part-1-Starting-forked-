3ece049cca2cd2565f1ffeb33e60c89daa3ed97a
Daniel Akoko <tobiakoko@gmail.com>
2023-11-27 20:10:39 -0500

    HTMl, CSS && React commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    return (
+        <body>
+            <div className="note">
+                <h1>This is the note title</h1>
+                <p>This is the note content</p>
+            </div>
+        </body>
+    );
+}
+
+export default Note;
